6dbc91d5ac37d69025cd538adaf5caabeafffb84
Fuad <fuadhaque66@gmail.com>
2026-07-23 07:57:59 +0600

    three sums returned empty

--- DSA/two-sum.py
+++ DSA/two-sum.py
@@ -8,3 +8,15 @@ def two_sum(nums, target):
     return []
 
 print(two_sum([2, 7, 11, 15], 9))
+
+
+
+def three_sum(numss, targett):
+    for i in range(len(numss)):
+        for j in range(i +1, len(numss)):
+            for k in range(j + 1, len(numss)):
+                if numss[i] + numss[j] + numss[k] == targett:
+                    return [i, j, k]
+    return []
+
+print(three_sum([8, 9, 44, 55, 35, 45], 100))
