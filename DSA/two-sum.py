32109417a6663e072c148be40e364a2fdbefca84
Fuad <fuadhaque66@gmail.com>
2026-07-23 07:53:02 +0600

    two sum problem

---
+++ DSA/two-sum.py
@@ -0,0 +1,10 @@
+def two_sum(nums, target):
+    seen = {}
+    for i, x in enumerate(nums):
+        complement = target - x
+        if complement in seen:
+            return [seen[complement], i]
+        seen[x] = i
+    return []
+
+print(two_sum([2, 7, 11, 15], 9))
